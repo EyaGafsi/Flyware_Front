f87935fc4e928a46fee2221ed103ee6bcc6ce9d1
eya <eyagafsi17@gmail.com>
2024-01-08 09:27:11 +0100

    username

--- src/app/views/_auth/auth.guard.ts
+++ src/app/views/_auth/auth.guard.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
-import {  ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class AuthGuard extends KeycloakAuthGuard {
-  constructor(protected override readonly router: Router,protected readonly keycloak: KeycloakService) {
-    super(router,keycloak)
+  constructor(protected override readonly router: Router, protected readonly keycloak: KeycloakService) {
+    super(router, keycloak);
   }
+
   public async isAccessAllowed(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if(!this.authenticated){
+    if (!this.authenticated) {
       await this.keycloak.login({
-        redirectUri:window.location.origin
+        redirectUri: window.location.origin,
       });
     }
-    const requiredRoles =route.data['roles'];
 
-    if(!(requiredRoles instanceof Array)||requiredRoles.length===0){
+    const requiredRoles = route.data['roles'];
 
+    if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
       return true;
     }
 
-    return requiredRoles.every((role)=>this.roles.includes(role));
-}
-}
+    const hasRequiredRoles = requiredRoles.every((role) => this.roles.includes(role));
 
+    if (!hasRequiredRoles) {
+      this.router.navigate(['/accessDenied']);
+      return false;
+    }
 
+    return true;
+  }
+}
